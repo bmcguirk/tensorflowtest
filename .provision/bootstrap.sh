b9c8c3beba7ca2524a9b76e557789dacf981910a
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-07-14 16:27:55 -0400

    Change python installation method.

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -18,9 +18,9 @@ sudo rm -Rf /var/www
 ln -s /vagrant /var/www
 
 # Download and unzip Python 3.5.2
-wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
-gunzip Python-3.5.2.tgz
-tar -xf Python-3.5.2.tar
+# wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
+# gunzip Python-3.5.2.tgz
+# tar -xf Python-3.5.2.tar
 
 # Install openssl-dev
 sudo apt-get -y install openssl-dev
@@ -29,17 +29,19 @@ sudo apt-get -y install openssl-dev
 sudo apt-get -y install make
 
 # Build Python 3.5.2
-cd Python-3.5.2
-sudo ./configure --with-ensurepip=install
-sudo make
-sudo make install
+# cd Python-3.5.2
+# sudo ./configure --with-ensurepip=install
+# sudo make
+# sudo make install
+sudo apt-get -y install python3 pip3
 python3 --version
 
-CFLAGS='-O2'
+# Supposedly this speeds up Python...
+# CFLAGS='-O2'
 
 # Get Tensorflow
 # Ubuntu/Linux 64-bit, CPU only, Python 3.5
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp35-cp35m-linux_x86_64.whl 
 
-# Install pelican, markdown
+# Install Tensorflow.
 sudo pip3 install --upgrade $TF_BINARY_URL
