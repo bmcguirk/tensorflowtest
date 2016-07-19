432ffc5c994bb329ffde39f8e04721cee857a4cd
Brian J. McGuirk <brian.mcguirk@ode.ri.gov>
2016-07-19 10:49:11 -0400

    Add base ty.py script.

--- .provision/bootstrap.sh
+++ .provision/bootstrap.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Supposedly this speeds up Python...
+CFLAGS='-O2'
+
 # nginx
 sudo apt-get update
 sudo apt-get -y install nginx
@@ -22,26 +25,35 @@ ln -s /vagrant /var/www
 # gunzip Python-3.5.2.tgz
 # tar -xf Python-3.5.2.tar
 
-# Install openssl-dev
-sudo apt-get -y install openssl-dev
-
 # Get and install make
 sudo apt-get -y install make
 
+## Commenting out to try and use apt-get.
 # Build Python 3.5.2
 # cd Python-3.5.2
 # sudo ./configure --with-ensurepip=install
 # sudo make
 # sudo make install
-sudo apt-get -y install python3 pip3
+
+# Install and verify Python version.
+sudo apt-get -y install python3 python3-pip
 python3 --version
 
-# Supposedly this speeds up Python...
-# CFLAGS='-O2'
+
 
 # Get Tensorflow
-# Ubuntu/Linux 64-bit, CPU only, Python 3.5
-export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp35-cp35m-linux_x86_64.whl 
+# Ubuntu/Linux 64-bit, CPU only, Python 3.5 - not working
+# export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp35-cp35m-linux_x86_64.whl 
+# Python 3.4 - current version of Python installed by apt-get.
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp34-cp34m-linux_x86_64.whl
 
 # Install Tensorflow.
 sudo pip3 install --upgrade $TF_BINARY_URL
+
+# Install Protobuf.
+sudo pip3 install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/protobuf-3.0.0b2.post2-cp34-none-linux_x86_64.whl
+
+# Print TF directory.
+python3 -c 'import os; import inspect; import tensorflow; print(os.path.dirname(inspect.getfile(tensorflow)))'
+
+# The above script ACTUALLY works, after a few iterations.
